98e3250aad48b66510b5c2e9d64d2aca5db9f902
princegobah <babynamek@gmail.com>
2022-01-31 18:36:44 -0500

    fetch test

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
 
-
-
-
-
-
-
-
-  const fetchTest=document.addEventListener("DOMContentLoaded", function(){
-    fetch(" http://localhost:3000/houses")
-    .then(res=>res.json())
-    .then(callB=>renderHouses(callB))})
-
-
-function renderHouses(houses) {
-    const div = document.querySelector('div');
+ const fetchTest=document.addEventListener("DOMContentLoaded", function(){
+  fetch(" http://localhost:3000/houses")
+  .then(res=>res.json())
+  .then(callB=>renderHouses(callB))})
 
   
    
-   
-    houses.forEach(house => {const list = document.createElement('li');
-    list.className='lists'
-  list.innerHTML = `
-  <h2>${house.name}</h2>
-  <h3>${house.region}</h3>
-  <img src=${house.img}
-  alt='easter egg'
-  width='100'
-  height='150'>
-  <p class='price'>$25</p>
-  <button class='buyer'>buy</button>`
-  div.appendChild(list);
-
-        
-  
-    });
-     let test=document.querySelectorAll('.buyer')
-    test.forEach(item=>{item.addEventListener('click',(e)=>{let subMoney=document.querySelector('.wallet')
- subMoney.innerHTML-=25})})}
-   
-     
- 
-     
-   
-     
-    
-
