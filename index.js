3fac8a84242a64e768b3f654e80fcbf64d882cf3
princegobah <babynamek@gmail.com>
2022-01-31 18:38:32 -0500

    created cards

--- index.js
+++ index.js
@@ -4,5 +4,43 @@
   .then(res=>res.json())
   .then(callB=>renderHouses(callB))})
 
+  function renderHouses(houses) {
+    const div = document.querySelector('.card');
+ 
+    const money=document.querySelector('.wallet')
   
+    //money.textContent=wallet
+
+ 
+   
+    houses.forEach(house => {const list = document.createElement('li');
+   
+    
+    
+    list.className='lists'
+  list.innerHTML = `
+  <h3 id=${house.id} class='header'>${house.name}</h3>
+  <h5>${house.region}</h5>
+  
+  <img src=${house.img}
+  alt='easter egg'
+  width='100'
+  height='150'>
+  <h6 class=bought>${house.isBought}</h6>
+  <p class='price'>${house.price}</p>
+  <button class='buyer'>buy</button>
+ <button id=sells class='seller'>sell</button>
+ <h7 class=left>${house.owned}</h7>
+  `  
+  div.appendChild(list);
+  
+  
+  
+}); 
+
+
+
+
+}
+
    
