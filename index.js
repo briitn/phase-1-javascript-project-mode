dc0cbbae357eb2c42176805ab173c1e7d7252eec
princegobah <babynamek@gmail.com>
2022-01-31 19:35:55 -0500

    Done

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
- const fetchTest=document.addEventListener("DOMContentLoaded", function(){
-  fetch(" http://localhost:3000/houses")
-  .then(res=>res.json())
-  .then(callB=>renderHouses(callB))})
-  const fetchWallet=document.addEventListener("DOMContentLoaded", function(){
-    fetch(" http://localhost:3000/wallet")
+  const fetchTest=document.addEventListener("DOMContentLoaded", function(){
+    fetch(" http://localhost:3000/houses")
     .then(res=>res.json())
-    .then(callB=>renderMoney(callB))})
-  function renderMoney(wallet){
-    const money=document.querySelector('.wallet')
-    money.textContent=wallet.holdMoney
-   }   
+    .then(callB=>renderHouses(callB))})
 
-  function renderHouses(houses) {
+    const fetchWallet=document.addEventListener("DOMContentLoaded", function(){
+      fetch(" http://localhost:3000/wallet")
+      .then(res=>res.json())
+      .then(callB=>renderMoney(callB))})
+     
+ function renderMoney(wallet){
+  const money=document.querySelector('.wallet')
+  money.textContent=wallet.holdMoney
+ }     
+
+function renderHouses(houses) {
     const div = document.querySelector('.card');
  
     const money=document.querySelector('.wallet')
@@ -39,34 +41,50 @@
   <button class='buyer'>buy</button>
  <button id=sells class='seller'>sell</button>
  <h7 class=left>${house.owned}</h7>
-  `  
+  `
+  
   div.appendChild(list);
   
   
   
-}); 
-const getBuyer=document.querySelectorAll('.buyer')
-let count=0
-getBuyer.forEach(item=>{
-  
-  item.addEventListener('click',(e)=>{const pricer=item.previousElementSibling.textContent
-const rePrice=pricer.replace( /^\D+/g, '')
-const getH6=item.previousElementSibling.previousElementSibling
+    }); 
+   
+
+
+    
+  const getBuyer=document.querySelectorAll('.buyer')
+  let count=0
+
+ getBuyer.forEach(item=>{
+   
+    item.addEventListener('click',(e)=>{const pricer=item.previousElementSibling.textContent
+  const rePrice=pricer.replace( /^\D+/g, '')
+ 
+  const getH6=item.previousElementSibling.previousElementSibling
  const geth7=item.nextElementSibling .nextElementSibling
- const header=
- item.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling
+
 if(geth7.textContent<=4){
   count=geth7.innerHTML++}else{geth7.textContent=5}
+  console.log(geth7)
+const header=
+item.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling
+
 if (count<=5){
-  if(item.previousElementSibling.previousElementSibling.textContent!=='owned (5/5 houses'){
+if(item.previousElementSibling.previousElementSibling.textContent!=='owned (5/5 houses)'){
+   
 if (money.textContent>0){
-  if (confirm(`Are you sure you want to buy ${header.textContent} for ${pricer}?`)===true){fetch('http://localhost:3000/wallet',{method:"PATCH",
+  if (confirm(`Are you sure you want to buy ${header.textContent} for ${pricer}?`)===true){ 
+ fetch('http://localhost:3000/wallet',{method:"PATCH",
 headers:{'Content-type':"application/json",
 Accept:'application/json'},
 body:JSON.stringify({
-  holdMoney:money.textContent-rePrice})})
-  count=geth7.textContent
-  fetch(`http://localhost:3000/houses/${header.id}`,{
+  holdMoney:money.textContent-rePrice
+})
+
+})
+count=geth7.textContent
+console.log(count)
+fetch(`http://localhost:3000/houses/${header.id}`,{
         method:"PATCH",
         headers:{  "Content-Type": "application/json",
         Accept: "application/json"
@@ -79,24 +97,44 @@ body:JSON.stringify({
       }).then(res=>res.json())
       .then(obj=>{createH6(obj)
       })
-  function createH6(house){
-    getH6.innerHTML=house.isBought;
-  geth7.innerHTML=house.owned}
-}}else {alert('sorry you dont have enough money')}}
+
+      function createH6(house){
+        getH6.innerHTML=house.isBought;
+      geth7.innerHTML=house.owned}
+console.log(money.textContent)
+item.nextElementSibling.disabled=false
+
+const reMoney=money.innerHTML-=rePrice
+
+}
+
+
+}
+else {alert('sorry you dont have enough money')}}
 else {alert("you own all houses")}}
 
 
-})})}
+
+
+
+
+
+})})
 const getSell2=document.querySelectorAll('.seller')
 getSell2.forEach(item=>{
-item.addEventListener("click",(e)=>{let geth7=item.nextElementSibling
-
-  let count=geth7.textContent
+item.addEventListener("click",(e)=>{
+  let geth7=item.nextElementSibling
+console.log(geth7)
+let count=geth7.textContent
+  console.log(count)
   const header=item.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.id
-  if (count>0){
-  if (confirm(`are you sure you want to sell?`)==true){
-    count--
-    {fetch(`http://localhost:3000/houses/${header}`,{
+console.log(header)
+
+if (count>0){
+  if (confirm(`are you sure you want to sell?`)==true)
+  count--
+  console.log(count)
+  {fetch(`http://localhost:3000/houses/${header}`,{
     method:"PATCH",
     headers:{  "Content-Type": "application/json",
     Accept: "application/json"
@@ -109,15 +147,26 @@ item.addEventListener("click",(e)=>{let geth7=item.nextElementSibling
   }).then(res=>res.json())
   .then(obj=>{createH6(obj)
   })
-  }} const getH6=item.previousElementSibling.previousElementSibling.previousElementSibling
+  const getH6=item.previousElementSibling.previousElementSibling.previousElementSibling
   
+ 
   function createH6(house){
     getH6.innerHTML=house.isBought
    geth7.innerHTML=house.owned}
-   const pricer=item.previousElementSibling.previousElementSibling.textContent
-   const rePrice=pricer.replace( /^\D+/g, '')
-   money.innerHTML=parseInt(rePrice)+parseInt(money.textContent)
+console.log(getH6)
+}
+  const pricer=item.previousElementSibling.previousElementSibling.textContent
+  const rePrice=pricer.replace( /^\D+/g, '')
+  money.innerHTML=parseInt(rePrice)+parseInt(money.textContent)
 
-}else {alert('you do not own any houses')}})})
+  }else {alert('you do not own any houses')}
+  
 
-   
+}
+)})}
+
+
+
+
+
+
